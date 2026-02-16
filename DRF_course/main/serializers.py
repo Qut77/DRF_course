81e3b0fb009605fcf7a638f5e5ef03d477e161ec
Qut77 <93013931+Qut77@users.noreply.github.com>
2026-02-16 12:44:44 +0500

    feat: new apiview + serializer

--- DRF_course/main/serializers.py
+++ DRF_course/main/serializers.py
@@ -4,4 +4,4 @@ from .models import Trip
 class TripSerializer(serializers.ModelSerializer):
     class Meta:
         model = Trip
-        fields = ('title', 'destination')
+        fields = ('title', 'description', 'created_at', 'updated_at', 'is_visible', 'destination')
